3580421c62b9d1f5bb00479395679ca4e18c5614
Future_Internals <st3v3bl0ks@gmail.com>
2025-02-18 13:42:49 +0000

    Update UniversalControlUI.lua

--- UniversalControlUI.lua
+++ UniversalControlUI.lua
@@ -2,4 +2,8 @@
     The control UI is WIP
 ]]
 
+if not _G.Library then _G.Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/main.lua"))(); if not _G.UI_Library then _G.UI_Library = Library.UI.Fluent end end
+repeat task.wait(0.1) until _G.UI_Library ~= nil
+local function Notify(Message, SubMessage) if _G.UI_Library and typeof(Message) == "string" and typeof(SubMessage) == "string" then _G.UI_Library:Notify({Title = "Notification", Content = Message, SubContent = SubMessage, Duration = 6, Theme = _G.UI_Library.Themes.Amethyst}) end end
+
 Notify("The control UI is WIP", "SB_'s light-weight aimbot")
