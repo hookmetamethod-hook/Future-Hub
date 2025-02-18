8b92da7f38f26b64e38719870fe661fc26a1701f
SB_Scripts <st3v3bl0ks@gmail.com>
2025-02-18 18:51:15 +0000

    Update UniversalControlUI.lua

--- UniversalControlUI.lua
+++ UniversalControlUI.lua
@@ -2,7 +2,7 @@
     The control UI is WIP
 ]]
 
-if not _G.Library then _G.Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/main.lua"))(); if not _G.UI_Library then _G.UI_Library = _G.Library.UI.Fluent end end
+if not _G.Library then _G.Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/SB-Script/FutureLib/refs/heads/main/main.lua"))(); if not _G.UI_Library then _G.UI_Library = _G.Library.UI.Fluent end end
 repeat task.wait(0.1) until _G.UI_Library ~= nil
 local function Notify(Message, SubMessage) if _G.UI_Library and typeof(Message) == "string" and typeof(SubMessage) == "string" then _G.UI_Library:Notify({Title = "Notification", Content = Message, SubContent = SubMessage, Duration = 6, Theme = _G.UI_Library.Themes.Amethyst}) end end
 
