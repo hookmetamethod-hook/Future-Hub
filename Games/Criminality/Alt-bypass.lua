bbb5ac47655976b37e23024460945a51c1c4d2f5
SB_Scripts <st3v3bl0ks@gmail.com>
2025-02-20 17:29:47 +0000

    Create Alt-bypass.lua

---
+++ Games/Criminality/Alt-bypass.lua
@@ -0,0 +1,5 @@
+-- Full credit to criminology --
+--------------------------------
+
+if tostring(game.PlaceId) == "4588604953" then game:GetService("RunService").Heartbeat:Connect(function()game:GetService("ReplicatedStorage"):WaitForChild("Events").Play:InvokeServer("play", "Casual", nil, 1) end) end
+-- put in auto-execute folder or spam execute when u join the game.
