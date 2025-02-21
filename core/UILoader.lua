e5f6e261febd056d883260a2d1d5e6b49d1c8630
hookmetamethod__namecall <st3v3bl0ks@gmail.com>
2025-02-21 09:52:15 +0000

    Update UILoader.lua

--- core/UILoader.lua
+++ core/UILoader.lua
@@ -1,8 +1,16 @@
 if not _G.Library then
-    if _G.FHdebug then print("Library not loaded") end
     _G.Library = {}
     _G.Library.Main = loadstring(game:HttpGet("wip"))()
     _G.Library.Notifications = loadstring(game:HttpGet("https://raw.githubusercontent.com/hookmetamethod-hook/Future-Hub/refs/heads/main/core/NotificationLib.lua"))()
-    _G.Library.Logger = loadstring(game:HttpGet("https://raw.githubusercontent.com/hookmetamethod-hook/Future-Hub/refs/heads/main/core/Logger.lua"))
-    if _G.FHdebug then print("Loaded library") end
+    _G.Library.Log = function(Type, Text)
+        if not _G.LoggerCore then _G.LoggerCore = loadstring(game:HttpGet("https://raw.githubusercontent.com/hookmetamethod-hook/Future-Hub/refs/heads/main/core/Logger.lua"))() end
+        repeat task.wait(0.1) until _G.LoggerCore ~= nil
+        if not _G.Logger then _G.Logger = _G.LoggerCore:Window({Title = "[Future Hub] Debugger", Position = UDim2.new(0.5, 0, 0.5, 0), DragSpeed = 12}) end
+        repeat task.wait(0.1) until _G.Logger ~= nil
+        _G.Logger:Prompt({
+            Title = Text,
+            TypesWeHave = {"default", "success", "fail", "warning", "nofitication"},
+            Type = Type
+        })
+    end
 end
