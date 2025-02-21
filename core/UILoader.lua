daa0b81d29d0bb53ea2b6d29d6fb50b9c411f706
hookmetamethod__namecall <st3v3bl0ks@gmail.com>
2025-02-21 09:24:02 +0000

    Update UILoader.lua

--- core/UILoader.lua
+++ core/UILoader.lua
@@ -1,8 +1,8 @@
 if not _G.Library then
     if _G.FHdebug then print("Library not loaded") end
     _G.Library = {}
-    _G.Library.Main = loadstring(game:HttpGet("wip"))
-    _G.Library.Notifications = loadstring(game:HttpGet("https://raw.githubusercontent.com/hookmetamethod-hook/Future-Hub/refs/heads/main/core/NotificationLib.lua"))
+    _G.Library.Main = loadstring(game:HttpGet("wip"))()
+    _G.Library.Notifications = loadstring(game:HttpGet("https://raw.githubusercontent.com/hookmetamethod-hook/Future-Hub/refs/heads/main/core/NotificationLib.lua"))()
     _G.Library.Logger = loadstring(game:HttpGet("https://raw.githubusercontent.com/hookmetamethod-hook/Future-Hub/refs/heads/main/core/Logger.lua"))
     if _G.FHdebug then print("Loaded library") end
 end
