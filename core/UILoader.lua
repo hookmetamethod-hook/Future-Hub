4feda5d205e8ba43cb9f10de374cae5d9d79b355
hookmetamethod__hook <st3v3bl0ks@gmail.com>
2025-02-21 17:10:49 +0000

    Update UILoader.lua

--- core/UILoader.lua
+++ core/UILoader.lua
@@ -7,13 +7,5 @@ if not _G.Library then
     _G.Library = {}
     _G.Library.Main = loadstring(game:HttpGet("https://raw.githubusercontent.com/hookmetamethod-hook/Future-Hub/refs/heads/main/core/UIMain.lua"))()
     _G.Library.Notifications = loadstring(game:HttpGet("https://raw.githubusercontent.com/hookmetamethod-hook/Future-Hub/refs/heads/main/core/NotificationLib.lua"))()
-    _G.Library.Log = function(Type, Text)
-        if not _G.Logger then _G.Logger = _G.LoggerCore:Window({Title = "[Future Hub] Debugger", Position = UDim2.new(0.5, 0, 0.5, 0), DragSpeed = 12}) end
-        repeat task.wait(0.1) until _G.Logger ~= nil
-        _G.Logger:Prompt({
-            Title = Text,
-            TypesWeHave = {"default", "success", "fail", "warning", "nofitication"},
-            Type = Type
-        })
-    end
+    _G.Library.Logger = _G.LoggerCore:Window({Title = "[Future Hub] Debugger", Position = UDim2.new(0.5, 0, 0.5, 0), DragSpeed = 12})
 end
