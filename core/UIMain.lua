2e153b0315ef0bc8c28c74c0a7c07fa92411f27a
hookmetamethod__hook <st3v3bl0ks@gmail.com>
2025-02-22 18:17:43 +0000

    Update UIMain.lua

--- core/UIMain.lua
+++ core/UIMain.lua
@@ -649,7 +649,7 @@ function library:Init(key)
     end
 
     local title = library.title
-    key = key or Enum.KeyCode.RightAlt
+    key = key or Enum.KeyCode.RightShift
 
     local screen = Instance.new("ScreenGui")
     local edge = Instance.new("Frame")
