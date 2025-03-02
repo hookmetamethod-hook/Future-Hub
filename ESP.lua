d76fceee577f33ce829a53dd6cbbad6862520ed6
userbase <samjamesleathes+github@gmail.com>
2025-03-02 13:51:45 +0000

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,45 +1,46 @@
-_G.ESPStatus = "Loading"
-
 _G.ESPEnabled = false
 _G.ESPColour = Color3.fromRGB(255, 255, 255)
 
+local highlight = Instance.new("Highlight")
+
+for _, v in pairs(game.Players:GetPlayers()) do
+	if not v.Character:FindFirstChild("Highlight") then
+		highlight:Clone().Parent = v.Character
+		highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
+		highlight.FillColor = _G.ESPColour
+	end
+end
+
 pcall(
 	function()
-		local highlight = Instance.new("Highlight")
-
-		game:GetService("RunService").RenderStepped:Connect(
-			function()
+		game.Players.PlayerAdded:Connect(
+			function(plr)
 				for _, v in pairs(game.Players:GetPlayers()) do
 					if not v.Character:FindFirstChild("Highlight") then
 						highlight:Clone().Parent = v.Character
 						highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
 						highlight.FillColor = _G.ESPColour
 					end
-
-					game.Players.PlayerAdded:Connect(
-						function(plr)
-							plr.CharacterAdded:Connect(
-								function(char)
-									if not char:FindFirstChild("Highlight") then
-										highlight:Clone().Parent = char
-										highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
-										highlight.FillColor = _G.ESPColour
-									end
-								end
-							)
+				end
+				plr.CharacterAdded:Connect(
+					function(char)
+						if not char:FindFirstChild("Highlight") then
+							highlight:Clone().Parent = char
+							highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
+							highlight.FillColor = _G.ESPColour
 						end
-					)
-				end
-
-				for _,v in pairs(game.Players:GetPlayers()) do
-					local CharHighlight = v.Character:FindFirstChild("Highlight")
-					CharHighlight.Enabled = _G.ESPEnabled
-					CharHighlight.OutlineColor = Color3.fromRGB(255, 255, 255)
-					CharHighlight.FillColor = _G.ESPColour
-				end
+					end
+				)
 			end
 		)
 	end
 )
-
-_G.ESPStatus = "Loaded"
+while true do
+	for _,v in pairs(game.Players:GetPlayers()) do
+		local CharHighlight = v.Character:FindFirstChild("Highlight")
+		CharHighlight.Enabled = _G.ESPEnabled
+		CharHighlight.OutlineColor = Color3.fromRGB(255, 255, 255)
+		CharHighlight.FillColor = _G.ESPColour
+	end
+	wait(0.3)
+end
